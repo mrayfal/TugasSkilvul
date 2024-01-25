d41adfb952aade2386f35321584a12b329f789f5
rayfal <mrayfal21@gmail.com>
2024-01-25 14:12:16 +0700

    Javascript Operator Javascript

--- tugasJS.js
+++ tugasJS.js
@@ -1,4 +1,4 @@
-// Variabel Dan Tipe Data
+// Variabel Dan Tipe Data//
 
 //Mendeklarasikan Variable
 let animal;
@@ -6,3 +6,20 @@ let animal;
 //Tipe Data Javascript
 let namaSaya = "Rehan";
 const umurSaya = 10 ;
+
+//Operator Javascript//
+
+//Operator Aritmatika
+const penjumlahan = 10 + 5;
+const modulus = penjumlahan % 4;
+const pangkat = modulus ** 5;
+
+//Assignment Operator
+let umur = 10;
+
+umur += 5;
+
+//String Operator
+let kalimat = 'Hello ';
+
+kalimat += 'World';
