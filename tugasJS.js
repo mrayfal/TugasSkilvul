649a406236447114bc3314205de7c5394b05dcb3
rayfal <mrayfal21@gmail.com>
2024-01-25 13:41:37 +0700

    Javascript 1 Array

---
+++ tugasJS.js
@@ -0,0 +1,10 @@
+//Tugas Javascript Dasar 1 Array
+
+//Mendeklarasikan Array
+let hewan = ["monyet"];
+
+//Mengubah Data Atau Element Pada Array
+let warna = ["hitam", "merah", "ungu", "kuning"];
+
+warna[3] = "biru";
+
